65a36f7dd5797760523bf5aa90bd670437b5f8c2
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2024-01-10 11:40:52 +0800

    Add ImportContactRequest class for managing contact imports

---
+++ backend/Requests/Contacts/ImportContactRequest.cs
@@ -0,0 +1,34 @@
+using ConnectApi.Entities;
+
+namespace ConnectApi.Imports;
+
+public class ImportContactRequest
+{
+    public byte[]? Image { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string? LastName { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public string? Email { get; set; }
+
+    public DeliveryAddress? DeliveryAddress { get; set; }
+
+    public BillingAddress? BillingAddress { get; set; }
+
+    public string? Website { get; set; }
+
+    public string? Notes { get; set; }
+
+    public bool IsFavorite { get; set; } = false;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public string? AppUserId { get; set; }
+
+    public List<Tag> Tags { get; set; } = new();
+}
